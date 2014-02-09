0f0451d5081fc3a8f35f3f1d7b7ee091a3c46050
Rickard Liljeros <rickard@liljerosdesign.se>
2014-02-09 19:27:52 +0100

    First commit

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -5,6 +5,6 @@ class CCIndex implements IController {
 
 	public function Index(){
 		global $rd;
-		$rd->data['title'] = "The Index Controller";
+		$rd->data['title'] = "The Index Controller - Rickard Liljeros";
 	}
 }
